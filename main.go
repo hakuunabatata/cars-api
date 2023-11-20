4a09244028617895c9287f1eeaa130b1a3445919
hakuunabatata <emachadomartins1999@gmail.com>
2023-11-20 12:10:12 -0300

    create sum

--- main.go
+++ main.go
@@ -1,7 +1,23 @@
 package main
 
-import "net/http"
+import (
+	"fmt"
+)
 
 func main() {
-	http.ListenAndServe(":8080", nil)
+	a := 10
+	b := 20
+	result, err := sum(a, b)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println(result)
+}
+
+func sum(a int, b int) (int, error) {
+	if a+b > 10 {
+		return 0, fmt.Errorf("sum > 10")
+	}
+	return a + b, nil
 }
