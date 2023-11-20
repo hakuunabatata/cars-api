88f7e9c44a24068e904eb3c280593fbcb9fc18ea
hakuunabatata <emachadomartins1999@gmail.com>
2023-11-20 13:47:19 -0300

    add sqlite to api

--- main.go
+++ main.go
@@ -1,12 +1,15 @@
 package main
 
 import (
-	"github.com/labstack/echo/v4"
+	sql "database/sql"
+
+	echo "github.com/labstack/echo/v4"
+	_ "github.com/mattn/go-sqlite3"
 )
 
 type Car struct {
-	Name  string
-	Price float64
+	Name  string  `json:"name"`
+	Price float64 `json:"price"`
 }
 
 var cars []Car
@@ -16,11 +19,9 @@ func createCars(c echo.Context) error {
 	if err := c.Bind(car); err != nil {
 		return err
 	}
-
 	cars = append(cars, *car)
-
+	saveCar(*car)
 	return c.JSON(200, cars)
-
 }
 
 func getCars(c echo.Context) error {
@@ -32,4 +33,21 @@ func main() {
 	e.GET("/cars", getCars)
 	e.POST("/cars", createCars)
 	e.Start(":8080")
+}
+
+func saveCar(car Car) error {
+	db, err := sql.Open("sqlite3", "cars.db")
+	if err != nil {
+		return err
+	}
+	defer db.Close()
+	stmt, err := db.Prepare("INSERT INTO cars (name,price) VALUES ($1,$2)")
+	if err != nil {
+		return err
+	}
+	_, err = stmt.Exec(car.Name, car.Price)
+	if err != nil {
+		return err
+	}
+	return nil
 }
